931ea6efa56510538b0fdb690571b7fa85207e5a
Herykurniawann <herykurniawansartono@gmail.com>
2023-05-09 14:58:32 +0700

    add dequeue and dequeue

--- bab_8_stack_dan_queue/queue/enqueue.py
+++ bab_8_stack_dan_queue/queue/enqueue.py
@@ -15,7 +15,23 @@ class Queue:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+            
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else :
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1 
+        
+my_queue = Queue(1)
 
-my_queue = Queue(4)
+print("Queue before enqueue(2)")
+my_queue.print_queue()
 
+my_queue.enqueue(2)
+
+print("\nQueue after enqueue(2):")
 my_queue.print_queue()
