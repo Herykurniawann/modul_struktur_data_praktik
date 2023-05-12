a7db6ae47c211c3895dd8b4454edfa7cc8b55bff
Herykurniawann <herykurniawansartono@gmail.com>
2023-05-12 17:15:26 +0700

    complete insert BST

--- bab_9_binary_search_tree/insert.py
+++ bab_9_binary_search_tree/insert.py
@@ -18,4 +18,25 @@ class BinarySearchTree:
         while (True):
             if new_node.value == temp.value:
                 return False
-         
+            if new_node.value < temp.value == temp.value :
+                if temp.left is None :
+                    temp.left = new_node
+                    return True 
+                temp = temp.left
+                
+            else :
+                if temp.right is None :
+                    temp.right = new_node
+                    return True 
+                temp = temp.right 
+                
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+
+print("Root : " , my_tree.root.value)
+print("Root --> Left : ", my_tree.root.left.value)
+print("Root --> Right : " , my_tree.root.right.value) 
