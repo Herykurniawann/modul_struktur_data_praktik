680df2e9446bc1a3b715c033955cd02093212058
Herykurniawann <herykurniawansartono@gmail.com>
2023-06-06 15:40:57 +0700

    update

--- bab_13_algoritma_pengurutan_lanjutan/merge.py
+++ bab_13_algoritma_pengurutan_lanjutan/merge.py
@@ -20,20 +20,4 @@ def merge(list1,list2):
         
     return combined
 
-def merge_sort(list):
-    if len(list) == 1:
-        return list
-    mid_index = int(len(list)/2)
-    left = merge_sort(list[:mid_index])
-    right = merge_sort(list[mid_index:])
-    
-    return merge(left,right)
-
-
-original_list = [3,1,4,2]
-
-sorted_list = merge_sort(original_list)
-
-print("original list: ",original_list)
-
-print("\nsorted list: ",sorted_list)
+print(merge([1,2,7,8],[3,4,5,6]))
